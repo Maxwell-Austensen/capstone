ada728c975af21b88b893ae35c5b15e7c51a4102
Amy Ganz <alg638@nyu.edu>
2017-02-11 17:14:26 -0500

    create covariates

---
+++ gentrification_data_covariates.R
@@ -0,0 +1,115 @@
+library(tidyverse)
+library(haven)
+
+
+# Load files --------------------------------------------------------------
+
+ncdb_raw <- read_dta("J:/DEPT/REUP/Data/National/Neighborhood Change Database 2010/Clean/nyc_all_years_all_fields_long.dta")
+ncdb_raw %>% names %>% tolower
+
+# For decennial years the cpi adjustments are actually for the previous year.
+# "Consumer Price Index for All Urban Consumers (Current Series) without seasonal adjustments from the US Bureau of Labor Statistics over all major expenditure classes for the New York City metropolitan area"
+cpi <- tribble(
+  ~year, ~cpi_2016_base,
+  1980,    0.279839766,
+  1990,    0.495889735,
+  2000,     0.67207108,
+  2008,    0.914563439
+)
+
+tract_pcsa_xwalk <- read_csv("C:/Users/alg638/Documents/capstone/tract_pcsa_xwalk.csv", col_types = "ccc")
+
+
+# Inflation Adjustment ----------------------------------------------------
+
+ncdb_adj <- ncdb_raw %>% 
+  filter(year %in% c(1990, 2000, 2008, 2010)) %>% 
+  select(year, geo2010, numhhs, rntocc, aggrent, avhhin_n, fem4, fem9, fem14, 
+         fem17_a, fem24, fem29,  fem34, fem44, fem54, fem64, fem74, fem75, 
+         men4, men9, men14, men17_a, men24, men29, men34, men44, men54, men64, 
+         men74, men75, trctpop, educ16, educ12, educpp, povrat_n, povrat_d, numhhs, 
+         shrhsp_n, shrnhb_n, shrnhw_n, shrnha_n, shr_d) %>% 
+  left_join(cpi, by = "year") %>% 
+  mutate(geoid = as.character(geo2010)) %>% 
+  mutate_at(vars(aggrent, avhhin_n), funs("adj" = . * (1 / cpi_2016_base))) %>%
+  mutate(age_lt5 = fem4 + men4, 
+         age_5_17 = fem9 + fem14 + fem17_a + men9 + men14 + men17_a,
+         age_20_34 = fem24 + fem29 + fem34 + men24 + men29 + men34,
+         age_35_54 = fem44 + fem54 + men44 + men54, 
+         age_55p = fem64 + fem74 + fem75 + men64 + men74 + men75)
+ 
+
+# Make wide Tract and PCSa files ------------------------------------------
+
+pcsa_wide <- ncdb_adj %>% 
+  right_join(tract_pcsa_xwalk, by = "geoid") %>% 
+  group_by(year, pcsa, pcsa_name) %>% 
+  summarise_at(vars(avhhin_n_adj, numhhs, aggrent_adj, rntocc, trctpop, educ16, educ12, educpp, povrat_n, povrat_d, numhhs, 
+        shrhsp_n, shrnhb_n, shrnhw_n, shrnha_n, shr_d, age_lt5, age_5_17, age_20_34, age_35_54, age_55p), sum) %>% 
+  ungroup %>% 
+  mutate(avg_inc_adj = avhhin_n_adj / numhhs,
+         avg_rent_adj = aggrent_adj / rntocc, 
+         sh_pov = povrat_n / povrat_d, 
+         sh_col_ed = educ16 / educpp, 
+         sh_hs_ed = educ12 / educpp, 
+         sh_hisp = shrhsp_n / shr_d, 
+         sh_blk = shrnhb_n / shr_d, 
+         sh_wht = shrnhw_n / shr_d, 
+         sh_asian = shrnha_n / shr_d, 
+         sh_lt5 = age_lt5 / trctpop, 
+         sh_5_17 = age_5_17 / trctpop, 
+         sh_20_34 = age_20_34 / trctpop, 
+         sh_35_54 = age_35_54 / trctpop, 
+         sh_55p = age_55p / trctpop) %>% 
+  gather("var", "value", -pcsa, -pcsa_name, -year) %>% 
+  unite(var_year, var, year) %>% 
+  spread(var_year, value)
+
+#do same for tract level data? 
+tract_wide <- ncdb_adj %>% 
+  mutate(avg_inc_adj = avhhin_n_adj / numhhs,
+         avg_rent_adj = aggrent_adj / rntocc, 
+         sh_pov = povrat_n / povrat_d, 
+         sh_col_ed = educ16 / educpp, 
+         sh_hs_ed = educ12 / educpp, 
+         sh_hisp = shrhsp_n / shr_d, 
+         sh_blk = shrnhb_n / shr_d, 
+         sh_wht = shrnhw_n / shr_d, 
+         sh_asian = shrnha_n / shr_d, 
+         sh_lt5 = age_lt5 / trctpop, 
+         sh_5_17 = age_5_17 / trctpop, 
+         sh_20_34 = age_20_34 / trctpop, 
+         sh_35_54 = age_35_54 / trctpop, 
+         sh_55p = age_55p / trctpop) %>% 
+  gather("var", "value", -geoid, -year) %>% 
+  unite(var_year, var, year) %>% 
+  spread(var_year, value)
+
+
+
+# Create Gentrification indictor ------------------------------------------
+
+get_gent_vars <- function(.data) {
+  .data %>% 
+    mutate(p40_inc_1990 = quantile(avg_inc_adj_1990, 0.4, na.rm = TRUE),
+           low_inc_1990 = avg_inc_adj_1990 <= p40_inc_1990,
+           rent_chg_90_10 = (avg_rent_adj_2008 - avg_rent_adj_1990) / avg_rent_adj_1990,
+           p50_rent_chg = quantile(rent_chg_90_10, 0.5, na.rm = TRUE),
+           rapid_rent = rent_chg_90_10 > p50_rent_chg, 
+           pop_chg_90_00 = (trctpop_2000 - trctpop_1990)/trctpop_1990, 
+           pop_chg_00_10 = (trctpop_2010 - trctpop_2000)/trctpop_2000) %>% 
+    mutate(gent_status = case_when(
+      .$low_inc_1990 == FALSE ~ "High Income",
+      .$rapid_rent == TRUE    ~ "Gentrifying",
+      .$rapid_rent == FALSE   ~ "Non-Gentrifying",
+      TRUE                    ~ NA_character_))
+}
+
+
+tract_output <- get_gent_vars(tract_wide)
+
+pcsa_output <- get_gent_vars(pcsa_wide)
+
+
+write_csv(tract_output, "C:/Users/alg638/Documents/capstone/tract_gent.csv")
+write_csv(pcsa_output, "C:/Users/alg638/Documents/capstone/pcsa_gent.csv")
