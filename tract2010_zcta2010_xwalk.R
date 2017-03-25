5b7adf979826b96464c1b41664d516d307bb7fbc
Maxwell Austensen <maxwell.austensen@nyu.edu>
2017-03-25 02:53:44 -0400

    filter out 9999 zctas and 0 afacts

--- tract2010_zcta2010_xwalk.R
+++ tract2010_zcta2010_xwalk.R
@@ -6,6 +6,7 @@ xwalk_names <- read_csv("../Dropbox/capstone/tract2010_zcta2010_xwalk_geocorr12.
 tract2010_zcta2010_xwalk <- "../Dropbox/capstone/tract2010_zcta2010_xwalk_geocorr12.csv" %>% 
   read_csv(skip = 2, col_names = xwalk_names) %>% 
   mutate(tract10 = str_c(county, str_replace(tract, "\\.", ""))) %>% 
+  filter(zcta5 != 99999, afact > 0) %>% 
   select(tract10, zcta2010 = zcta5, afact)
 
-write_csv(tract2010_zcta2010_xwalk, "../Dropbox/capstone/tract2010_zcta2010_xwalk_geocorr12.csv")
+write_csv(tract2010_zcta2010_xwalk, "../Dropbox/capstone/tract2010_zcta2010_xwalk.csv")
