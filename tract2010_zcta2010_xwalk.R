dd04f4dd551eba1d8271686eb1e7698ba64bf215
Maxwell Austensen <maxwell.austensen@nyu.edu>
2017-03-24 23:51:14 -0400

    tract 2010 -> zcta 2010 crosswalk

---
+++ tract2010_zcta2010_xwalk.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(stringr)
+
+xwalk_names <- read_csv("../Dropbox/capstone/tract2010_zcta2010_xwalk_geocorr12.csv", n_max = 1) %>% names
+
+tract2010_zcta2010_xwalk <- "../Dropbox/capstone/tract2010_zcta2010_xwalk_geocorr12.csv" %>% 
+  read_csv(skip = 2, col_names = xwalk_names) %>% 
+  mutate(tract10 = str_c(county, str_replace(tract, "\\.", ""))) %>% 
+  select(tract10, zcta2010 = zcta5, afact)
+
+write_csv(tract2010_zcta2010_xwalk, "../Dropbox/capstone/tract2010_zcta2010_xwalk_geocorr12.csv")
