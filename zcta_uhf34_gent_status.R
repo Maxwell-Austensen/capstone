96348f3d00e0f215d5f7a326ad3eb0bece4b1a86
Maxwell Austensen <maxwell.austensen@nyu.edu>
2017-04-08 13:32:17 -0400

    add CHS uhf34 codes to uhf34 gentrification data

--- zcta_uhf34_gent_status.R
+++ zcta_uhf34_gent_status.R
@@ -10,11 +10,20 @@ if(length(new_packages)) install.packages(new_packages)
 
 library(tidyverse) # for tidy data manipulation
 
+
+# -------------------------------------------------------------------------
+
+
 zcta_uhf34_xwalk <- read_csv("../dropbox/capstone/crosswalks/zcta_uhf34_xwalk_raw.csv", col_types = "cccc") %>% 
   mutate(zcta = stringr::str_split(zcta, ",")) %>% 
   unnest(zcta)
 
 feather::write_feather(zcta_uhf34_xwalk, "../Dropbox/capstone/crosswalks/zcta_uhf34_xwalk.feather")
+
+
+# The CHS data uses a 1:34 number for uhf34, so need extra crosswalk
+chs_uhf34_codes <- read_csv("../dropbox/capstone/crosswalks/chs_uhf34_codes.csv", 
+                            col_types = cols_only(chs_uhf34 = "i", uhf34 = "c"))
 
 # the "totpop" vars come from the dartmouth data, which uses decennial 2010 - here we want the acs 06-10
 # there are 9 zctas in the xwalk that aren't in our sample (these are ones dropped for pop < 2000)
@@ -31,9 +40,10 @@ uhf_gent_status <- feather::read_feather("../dropbox/capstone/data_clean/all_dat
                                           "Non-Gentrifying" = "nongent", 
                                           "Gentrifying" = "gent")) %>% 
   select(uhf34, uhf34_name, gent_short, gent_pop_pct) %>% 
-  spread(gent_short, gent_pop_pct, fill = 0)
+  spread(gent_short, gent_pop_pct, fill = 0) %>% 
+  left_join(chs_uhf34_codes, by = "uhf34")
 
 
-feather::write_feather(uhf_gent_status, "../Dropbox/capstone/data_inter/uhf_gent_status.feather")
+feather::write_feather(uhf_gent_status, "../Dropbox/capstone/data_inter/uhf34_gent_status.feather")
 
   
